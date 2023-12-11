0bc0b53b52a0f9bb33488ac1085700943ac83c80
aniketh <anikethbabu@gmail.com>
2023-12-11 18:34:34 -0500

    feat: Add celery configuration to django

--- src/authors_api/settings/local.py
+++ src/authors_api/settings/local.py
@@ -1,13 +1,20 @@
-from .base import * #noqa
+from .base import *  # noqa
 from .base import env
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env(
     "DJANGO_SECRET_KEY",
     default="-kQO9yGUK9x0yB4ncai-17oExb8GaZdeyQ-lyW1UcCZYewJc6fM",
-    )
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+CSRF_TRUSTED_ORIGINS = ["http://localhost:8080"]
 
+EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
+EMAIL_HOST = env("EMAIL_HOST", default="mailhog")
+EMAIL_PORT = env("EMAIL_PORT")
+DEFAULT_FROM_EMAIL = "anikethbabu@gmail.com"
+DOMAIN = env("DOMAIN")
+SITE_NAME = "Authors Haven"
