1dca1e9a48b7e76a4b21e40b008c8c7d002af676
aniketh <anikethbabu@gmail.com>
2023-12-11 20:48:10 -0500

    feat: Add timestampmodel
    An abstract model field which uses uuid and psudo primary key.
    The model has created_at and updated_at fields which it orders by.

--- src/core_apps/common/models.py
+++ src/core_apps/common/models.py
@@ -1,3 +1,15 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class TimeStampedModel(models.Model):
+    """Model which has UUID field as id and psudo primary key as pkid. This model also has created_at and updated_at fields"""
+
+    pkid = models.BigAutoField(primary_key=True, editable=False)
+    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+        ordering = ["-created_at", "-updated_at"]
