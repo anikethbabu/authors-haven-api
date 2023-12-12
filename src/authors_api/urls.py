eb1c59bba62b2fcd16449b6d830ce7f59869eade
aniketh <anikethbabu@gmail.com>
2023-12-12 18:25:44 -0500

    feat: Added JWT authentication and other auth routes

--- src/authors_api/urls.py
+++ src/authors_api/urls.py
@@ -4,6 +4,8 @@ from django.urls import path, include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
+from dj_rest_auth.views import PasswordResetConfirmView
+from core_apps.users.views import CustomUserDetailsView
 
 # Sets the schema view using drf_yasg openapi Info
 schema_view = get_schema_view(
@@ -21,6 +23,14 @@ urlpatterns = [
     # Sets redoc/ as path to a redoc schema view with the ui and no caching.
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0)),
     path(settings.ADMIN_URL, admin.site.urls),
+    path("api/v1/auth/user/", CustomUserDetailsView.as_view(), name="user"),
+    path("api/v1/auth/", include("dj_rest_auth.urls")),
+    path("api/v1/auth/registration/", include("dj_rest_auth.registration.urls")),
+    path(
+        "api/v1/auth/password/reset/confirm/<uidb64>/<token>/",
+        PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
 ]
 
 admin.site.site_header = "Authors Haven API Admin"
